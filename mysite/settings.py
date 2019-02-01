f2b00ec78e25f4386d1b3c645c9b12069fb68ee8
ismaina <maina.wanjau@gmail.com>
2019-02-01 19:19:56 +0300

    Changed requirements file and settings file:host

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'mwanjau.pythonanywhere.com',
+    'mainawanjau.pythonanywhere.com',
 ]
 
 MIDDLEWARE = [
